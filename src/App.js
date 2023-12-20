56d0c8e62e28f8af666ccc882d72057868e17039
AmanAsharma070320 <asharma070320@gmail.com>
2023-12-20 21:51:21 +0530

    Day 3 ClassWork

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Component/Navbar';
+import Main from './Component/Main';
+import Main2 from './Component/Main2';
+import Cards from './Component/Cards';
+// import Footer from './Component/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+
+      <Navbar/>      
+
+      <Main />
+
+      <Main2  mainTxt="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, debitis." mainPara="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, molestiae! Quidem est esse numquam odio deleniti, beatae, magni dolores provident quaerat totam eos, aperiam architecto eius quis quibusdam fugiat dicta." mainBtn="Get in Touch"/>
+
+      <Cards />
+      
+
     </div>
   );
 }
---
+++ src/Component/Cards.js
@@ -0,0 +1,28 @@
+function Cards() {
+  return (
+    <div class="cardsContainer">
+      <div class="Fcard">
+        <p class="cardCenter">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima
+          dicta amet, molestiae aliquam incidunt suscipit recusandae labore
+          ratione doloremque, architecto et illo minus quo tenetur ducimus,
+          voluptatibus repellendus fuga aperiam vel ab! Ipsam corrupti
+          blanditiis dolorum! Officia assumenda rem nam, eveniet enim ad
+          inventore laudantium est illum voluptatem quis.
+        </p>
+
+      
+        
+      </div>
+      <div class="cardsTxt">
+        <img class="cardsTxtImg" src="https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80" />
+
+     <div class="headSeo">
+        <h5>Gladis Lennon</h5>
+        <p>Head of SEO</p>
+        </div>
+        </div>
+     </div>
+  );
+}
+export default Cards;
